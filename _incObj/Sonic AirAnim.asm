a89fd9d187fd0f01c5dc193df56f23e921b6368e
Leo40Git <kfir40mailminer@gmail.com>
2018-01-06 19:18:50 +0200

    Improve air animation, add CD life counter icon

--- _incObj/Sonic AirAnim.asm
+++ _incObj/Sonic AirAnim.asm
@@ -7,11 +7,21 @@
 Sonic_AirAnim:
 		btst	#2,obStatus(a0)	; is Sonic rolling?
 		bne.s	.return		; if yes, branch
+		move.w	obVelX(a0),d0
+		tst.w	d0
+		bge.s	.gotvelx
+		neg.w	d0
+	.gotvelx:
+		cmpi.w	#$A00,d0	; is Sonic moving fast?
+		blt.s	.xslow		; if not, branch
+		move.b	#id_Walk,obAnim(a0)	; use walking animation
+		rts
+	.xslow:
 		tst.w	obVelY(a0)	; is Sonic going up?
 		ble.s	.goingup	; if yes, branch
 		; so, we're going down then
 		move.b	#id_Fall2,obAnim(a0)	; use falling animation
-		cmpi.w	#$600,obVelY(a0)	; is Sonic falling fast?
+		cmpi.w	#$800,obVelY(a0)	; is Sonic falling fast?
 		bcc.s	.return		; if yes, branch
 		move.b	#id_Fall,obAnim(a0)		; use slower falling animation
 		rts
