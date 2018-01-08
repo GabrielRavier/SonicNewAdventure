d913ae8be2c5385ff703892f514d961482d15313
Leo40Git <kfir40mailminer@gmail.com>
2018-01-08 16:31:36 +0200

    Sonic 3 monitors

--- _maps/Monitor.asm
+++ _maps/Monitor.asm
@@ -1,51 +1,46 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - monitors
-; ---------------------------------------------------------------------------
-Map_Monitor_internal:
-		dc.w .static0-Map_Monitor_internal
-		dc.w .static1-Map_Monitor_internal
-		dc.w .static2-Map_Monitor_internal
-		dc.w .eggman-Map_Monitor_internal
-		dc.w .sonic-Map_Monitor_internal
-		dc.w .shoes-Map_Monitor_internal
-		dc.w .shield-Map_Monitor_internal
-		dc.w .invincible-Map_Monitor_internal
-		dc.w .rings-Map_Monitor_internal
-		dc.w .s-Map_Monitor_internal
-		dc.w .goggles-Map_Monitor_internal
-		dc.w .broken-Map_Monitor_internal
-.static0:	dc.b 1			; static monitor
-		dc.b $EF, $F, 0, 0, $F0
-.static1:	dc.b 2			; static monitor
-		dc.b $F5, 5, 0,	$10, $F8
-		dc.b $EF, $F, 0, 0, $F0
-.static2:	dc.b 2			; static monitor
-		dc.b $F5, 5, 0,	$14, $F8
-		dc.b $EF, $F, 0, 0, $F0
-.eggman:	dc.b 2			; Eggman monitor
-		dc.b $F5, 5, 0,	$18, $F8
-		dc.b $EF, $F, 0, 0, $F0
-.sonic:		dc.b 2			; Sonic	monitor
-		dc.b $F5, 5, 0,	$1C, $F8
-		dc.b $EF, $F, 0, 0, $F0
-.shoes:		dc.b 2			; speed	shoes monitor
-		dc.b $F5, 5, 0,	$24, $F8
-		dc.b $EF, $F, 0, 0, $F0
-.shield:	dc.b 2			; shield monitor
-		dc.b $F5, 5, 0,	$28, $F8
-		dc.b $EF, $F, 0, 0, $F0
-.invincible:	dc.b 2			; invincibility	monitor
-		dc.b $F5, 5, 0,	$2C, $F8
-		dc.b $EF, $F, 0, 0, $F0
-.rings:		dc.b 2			; 10 rings monitor
-		dc.b $F5, 5, 0,	$20, $F8
-		dc.b $EF, $F, 0, 0, $F0
-.s:		dc.b 2			; 'S' monitor
-		dc.b $F5, 5, 0,	$3C, $F8
-		dc.b $EF, $F, 0, 0, $F0
-.goggles:	dc.b 2			; goggles monitor
-		dc.b $F5, 5, 0,	$38, $F8
-		dc.b $EF, $F, 0, 0, $F0
-.broken:	dc.b 1			; broken monitor
-		dc.b $FF, $D, 0, $30, $F0
+; --------------------------------------------------------------------------------
+; Sprite mappings - output from SonMapEd - Sonic 1 format
+; --------------------------------------------------------------------------------
+
+SME_WZ5M2:	
+		dc.w SME_WZ5M2_18-SME_WZ5M2, SME_WZ5M2_1E-SME_WZ5M2	
+		dc.w SME_WZ5M2_29-SME_WZ5M2, SME_WZ5M2_34-SME_WZ5M2	
+		dc.w SME_WZ5M2_3F-SME_WZ5M2, SME_WZ5M2_4A-SME_WZ5M2	
+		dc.w SME_WZ5M2_55-SME_WZ5M2, SME_WZ5M2_60-SME_WZ5M2	
+		dc.w SME_WZ5M2_6B-SME_WZ5M2, SME_WZ5M2_76-SME_WZ5M2	
+		dc.w SME_WZ5M2_81-SME_WZ5M2, SME_WZ5M2_8C-SME_WZ5M2	
+SME_WZ5M2_18:	dc.b 1	
+		dc.b $EF, $F, 0, 0, $F0	
+SME_WZ5M2_1E:	dc.b 2	
+		dc.b $F2, 5, 0, $10, $F8	
+		dc.b $EF, $F, 0, 0, $F0	
+SME_WZ5M2_29:	dc.b 2	
+		dc.b $F2, 5, 0, $14, $F8	
+		dc.b $EF, $F, 0, 0, $F0	
+SME_WZ5M2_34:	dc.b 2	
+		dc.b $F2, 5, 0, $18, $F8	
+		dc.b $EF, $F, 0, 0, $F0	
+SME_WZ5M2_3F:	dc.b 2	
+		dc.b $F2, 5, 0, $1C, $F8	
+		dc.b $EF, $F, 0, 0, $F0	
+SME_WZ5M2_4A:	dc.b 2	
+		dc.b $F2, 5, 0, $24, $F8	
+		dc.b $EF, $F, 0, 0, $F0	
+SME_WZ5M2_55:	dc.b 2	
+		dc.b $F2, 5, 0, $28, $F8	
+		dc.b $EF, $F, 0, 0, $F0	
+SME_WZ5M2_60:	dc.b 2	
+		dc.b $F2, 5, 0, $2C, $F8	
+		dc.b $EF, $F, 0, 0, $F0	
+SME_WZ5M2_6B:	dc.b 2	
+		dc.b $F2, 5, 0, $20, $F8	
+		dc.b $EF, $F, 0, 0, $F0	
+SME_WZ5M2_76:	dc.b 2	
+		dc.b $F5, 5, 0, $3C, $F8	
+		dc.b $EF, $F, 0, 0, $F0	
+SME_WZ5M2_81:	dc.b 2	
+		dc.b $F5, 5, 0, $38, $F8	
+		dc.b $EF, $F, 0, 0, $F0	
+SME_WZ5M2_8C:	dc.b 1	
+		dc.b $FF, $D, 0, $30, $F0	
 		even
